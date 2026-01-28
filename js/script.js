fa51c11ed04531827fbaeec814990854af3c3b7a
migppdev <pepegauser@proton.me>
2026-01-28 21:05:59 +0100

    Added JavaScript, initial popup modal design

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-//
+// ========= CREATE TASK =========
+
+// Create task button selectors
+const createTaskButton = document.querySelector(".new-task-button");
+const createTaskModal = document.querySelector("#new-task-modal");
+const closeTaskModal = document.querySelector("#close-task-modal");
+
+// Button & Modal listeners
+createTaskButton.addEventListener("click", () => {
+  createTaskModal.style.display = "block";
+});
+
+closeTaskModal.addEventListener("click", () => {
+  createTaskModal.style.display = "none";
+});
+
+// Edit task button selector
+const editTaskButton = document.querySelector(".edit-task-button");
+
+// Mark task as done button selector
+const taskDoneButton = document.querySelector(".task-done-button");
